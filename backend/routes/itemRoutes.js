7b3d99ca2b504b11db7590b79e3f4376019c00a0
Prakher <105749265+Prakher11@users.noreply.github.com>
2023-05-28 00:47:54 -0600

    added some error code in routes file
    login functionality left

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -9,9 +9,11 @@ router.get('/items', async (req, res) => {
     try {
         const items = await Item.find();
         res.json({items});
+        return res.status(200);
         
     } catch (error) {
         console.log(error);  
+        return res.status(500).json({ message: error.message });
     }
 });
 
@@ -22,16 +24,20 @@ router.post('/items', async (req, res) => {
         let item = await Item.findOne({Name});
 
         if(item){
-            res.json({items});
+            res.json({item});
+            
         }else{
             item = await Item.create({
                 Name, Price
             });
             res.json({item});
+            return res.status(200);
+           
         }
         
     } catch (error) {
         console.log(error);  
+        return res.status(500).json({ message: error.message });
     }
 });
 
@@ -39,11 +45,28 @@ router.delete('/items/:id', async (req, res) => {
     try {
       const { id } = req.params;
       const items = await Item.findOneAndDelete({ Id: id }); // Use Id instead of _id
+
+      if(!items){
+        return res.sendStatus(404);
+      }
       res.json({ items });
+      return res.sendStatus(200);
     } catch (error) {
       console.log(error);
+      return res.status(500).json({ message: error.message });
     }
   });
+
+router.delete('/items', async (req, res) => {
+    try {
+        // Logic to delete all items
+        await Item.deleteMany();
+        return res.send('All items deleted');
+      } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: error.message });
+      }
+})
 
 //router.post('/');
 //.get('/');
